3b41bb82af5e679217fc34ff7931cf485b417d4f
shiracollins100 <shiracollins100@gmail.com>
2026-08-09 14:48:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+
+#  ---  1  ---
+import module1
+def main():
+    module1.manage_library()
+main()
+
+
+
+
+#  ---  2  ---
+
+# module 1 : gets the input from user for the base numbers and the desired operation ( + - * / ** % ...)
+# module 2 : contains all the functions for the desired operation, a function for each operation
+# module 1: calls the fitting function from module 2 and returns the result/
+#
+#
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
